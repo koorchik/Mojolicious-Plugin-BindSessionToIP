e62ab44928f69586e2a25b1fbaf379797636f6fd
Viktor Turskyi <viktor@webbylab.com>
2012-07-14 16:30:28 +0300

    first lines

--- lib/Mojolicious/Plugin/BindSessionToIP.pm
+++ lib/Mojolicious/Plugin/BindSessionToIP.pm
@@ -2,9 +2,47 @@ package Mojolicious::Plugin::BindSessionToIP;
 use Mojo::Base 'Mojolicious::Plugin';
 
 our $VERSION = '0.01';
+use v5.10;
+use Data::Dumper;
 
 sub register {
-  my ($self, $app) = @_;
+    my ( $self, $app ) = @_;
+
+    $app->hook(
+        after_static_dispatch => sub {
+            my ($c) = @_;
+
+            my $stored_ip = $c->session('bind_session_to_ip.ip') // '';
+            my $req_ip    = $self->_get_ip($c);
+
+            if ( $stored_ip ne $req_ip ) {
+                if ($stored_ip) {
+                    my $path = $c->tx->req->url->to_abs->to_string;
+                    $c->app->log->debug("BindSessionToIP: Ip changed from [$stored_ip] to [$req_ip]");
+                }
+
+                $self->_destroy_session($c);
+                $c->session('bind_session_to_ip.ip' => $req_ip);
+            }
+
+            return 1;
+        },
+    );
+}
+
+sub _get_ip {
+    my ( $self, $c ) = @_;
+    return $c->tx->remote_address || $c->req->headers->header('X-Real-IP');
+}
+
+sub _destroy_session {
+    my ( $self, $c ) = @_;
+    my $session = $c->session;
+    #%$session = (); # Tests hags with this o_O
+    
+    foreach my $key (keys %$session) {
+        $session->{$key} = '';
+    } 
 }
 
 1;
@@ -12,7 +50,7 @@ __END__
 
 =head1 NAME
 
-Mojolicious::Plugin::BindSessionToIP - Mojolicious Plugin
+Mojolicious::Plugin::BindSessionToIP - Binds your Mojolicious session to IP-address for better security of your application
 
 =head1 SYNOPSIS
 
--- t/basic.t
+++ t/basic.t
@@ -1,16 +1,26 @@
 use Mojo::Base -strict;
 
-use Test::More tests => 3;
+use Test::More;
 
 use Mojolicious::Lite;
 use Test::Mojo;
 
 plugin 'BindSessionToIP';
 
-get '/' => sub {
-  my $self = shift;
-  $self->render_text('Hello Mojo!');
+get '/fill_session' => sub {
+    my $self = shift;
+    $self->session( 'status' => 'AUTHENTICATED' );
+    $self->render_text('DONE');
+};
+
+get '/check_session' => sub {
+    my $self = shift;
+    $self->render_text( $self->session( 'status') );
 };
 
 my $t = Test::Mojo->new;
-$t->get_ok('/')->status_is(200)->content_is('Hello Mojo!');
+$t->get_ok('/fill_session')->status_is(200)->content_is('DONE');
+$t->get_ok('/check_session')->status_is(200)->content_is('AUTHENTICATED');
+
+
+done_testing;
